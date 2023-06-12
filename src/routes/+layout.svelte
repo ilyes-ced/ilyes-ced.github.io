8ac4e1288c5d836c9b855551e0e7bc4bfdffca33
noot-noot-pengu <random_dude_233@proton.me>
2023-06-12 14:58:31 +0100

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+	import './styles.css';
+</script>
+
+<div class="app">
+	<main>
+		<slot />
+	</main>
+</div>
+
+<style>
+
+	main {
+		padding: 0px;
+	}
+
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,37 @@
+<div>
+	<div class="line">
+		<div class='username'>ilyes</div>
+		<div class='hostname'>@</div>
+		<div class='arobas'>ilyes-pc</div>
+		<div class='sign'>:~$</div>
+	</div>
+</div>
+
+
+<style>
+	div {
+		
+	}
+	.line {
+		font-size: large;
+		font-weight: 600;
+		font-family: "IBM Plex Mono", monospace;
+		/*background-color: white;
+		*/
+		display: flex;
+		flex-direction: row;
+		width: fit-content;
+	}
+	.line > .username {
+		color: rgb(37, 181, 37);
+	}
+	.line > .hostname {
+		color: greenyellow;
+	}
+	.line > .arobas {
+		color: rgb(37, 181, 37);
+	}
+	.line > .sign {
+		color: white;
+	}
+</style>
