0b44011db16e0af491b276494aab882a7122d7d8
ilyes-ced <random_dude_233@proton.me>
2025-10-20 01:11:07 +0000

    github preparation

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,18 @@ import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 import devtools from "solid-devtools/vite";
 import tailwindcss from "@tailwindcss/vite";
+import solid from "solid-start/vite";
+import staticAdapter from "solid-start-static";
 
 export default defineConfig({
-  plugins: [devtools(), solidPlugin(), tailwindcss()],
+  base: "/my-portfolio/",
+
+  plugins: [
+    devtools(),
+    solidPlugin(),
+    tailwindcss(),
+    solid({ adapter: staticAdapter() }),
+  ],
   server: {
     port: 3000,
   },
